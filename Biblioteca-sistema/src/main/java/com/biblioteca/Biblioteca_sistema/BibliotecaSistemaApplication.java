63b23dc13ebf1282f2cec0bb81596b2a3f9a2f2a
SamuelSSN-commits <SN0881390@gmail.com>
2025-06-15 16:38:05 -0300

    Add files via upload

---
+++ Biblioteca-sistema/src/main/java/com/biblioteca/Biblioteca_sistema/BibliotecaSistemaApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Biblioteca_sistema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaSistemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaSistemaApplication.class, args);
+	}
+
+}
---
+++ Biblioteca-sistema/src/test/java/com/biblioteca/Biblioteca_sistema/BibliotecaSistemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biblioteca.Biblioteca_sistema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaSistemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
