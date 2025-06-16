100c4fb4259dc772cdae61fb0641baed4b1b980e
Seu Nome <seuemail@example.com>
2025-06-16 04:48:59 +0000

    Primeiro commit - Adicionando o projeto Biblioteca-Sistema

---
+++ Biblioteca-sistema/src/main/java/com/biblioteca/Biblioteca_sistema/repository/LivroRepository.java
@@ -0,0 +1,12 @@
+package com.biblioteca.Biblioteca_sistema.repository;
+
+import com.biblioteca.Biblioteca_sistema.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long> {
+    // Você pode criar métodos customizados aqui, ex:
+    // Optional<Livro> findByIsbn(String isbn);
+}
+
